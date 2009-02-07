6ef5ef25837dd4432501231a79751af1b5452350
David Miller <dmiller@cdm.depaul.edu>
2009-02-06 23:06:42 -0600

    Added copyright notice to all files.

--- Clojure.Net/Clojure/Lib/RestFn.cs
+++ Clojure.Net/Clojure/Lib/RestFn.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/**
+ *   Copyright (c) Rich Hickey. All rights reserved.
+ *   The use and distribution terms for this software are covered by the
+ *   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+ *   which can be found in the file epl-v10.html at the root of this distribution.
+ *   By using this software in any fashion, you are agreeing to be bound by
+ * 	 the terms of this license.
+ *   You must not remove this notice, or any other, from this software.
+ **/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +22,6 @@ namespace clojure.lang
         protected readonly int _reqArity;
 
         #endregion
-
 
         #region C-tors
 
@@ -1367,7 +1376,6 @@ namespace clojure.lang
 
         #endregion
 
-
         #region Misc
 
         // do we need this?
@@ -1384,6 +1392,5 @@ namespace clojure.lang
         }
 
         #endregion
-
     }
 }
