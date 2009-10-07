fcda2cc0e06bcb470def3498fa8b01edbf21b4bf
David Miller <dmiller2718@gmail.com>
2009-10-07 14:03:02 -0500

    Removed named instantiations of ArrayStreamBase.  Added rank=1 check.

--- Clojure/Clojure/Lib/ArrayStream.cs
+++ Clojure/Clojure/Lib/ArrayStream.cs
@@ -16,7 +16,7 @@ using System.Text;
 namespace clojure.lang
 {
 
-    public abstract class ArrayStreamBase<T> : AFn
+    public class ArrayStreamBase<T> : AFn
     {
         #region Data
 
@@ -57,149 +57,53 @@ namespace clojure.lang
 
         public static Stream createFromObject(object array)
         {
-            Type aType = array.GetType().GetElementType();
+            Type aType = array.GetType();
+            if (!aType.IsArray)
+                throw new ArgumentException("Must be an array");
 
-            if (!aType.IsPrimitive)
+            if (((Array)array).Rank != 1)
+                throw new ArgumentException("Array must have rank 1");
+
+            Type eType = aType.GetElementType();
+
+            if (!eType.IsPrimitive)
                 return new Stream(new ArrayStream((Object[])array));
 
-            switch (Type.GetTypeCode(aType))
+            switch (Type.GetTypeCode(eType))
             {
                 case TypeCode.Char:
-                    return new Stream(new ArrayStream_char((char[])array));
+                    return new Stream(new ArrayStreamBase<char>((char[])array));
                 case TypeCode.SByte:
-                    return new Stream(new ArrayStream_sbyte((sbyte[])array));
+                    return new Stream(new ArrayStreamBase<sbyte>((sbyte[])array));
                 case TypeCode.Byte:
-                    return new Stream(new ArrayStream_byte((byte[])array));
+                    return new Stream(new ArrayStreamBase<byte>((byte[])array));
                 case TypeCode.Int16:
-                    return new Stream(new ArrayStream_short((short[])array));
+                    return new Stream(new ArrayStreamBase<short>((short[])array));
                 case TypeCode.Int32:
-                    return new Stream(new ArrayStream_int((int[])array));
+                    return new Stream(new ArrayStreamBase<int>((int[])array));
                 case TypeCode.Int64:
-                    return new Stream(new ArrayStream_long((long[])array));
+                    return new Stream(new ArrayStreamBase<long>((long[])array));
                 case TypeCode.Double:
-                    return new Stream(new ArrayStream_double((double[])array));
+                    return new Stream(new ArrayStreamBase<double>((double[])array));
                 case TypeCode.Single:
-                    return new Stream(new ArrayStream_float((float[])array));
+                    return new Stream(new ArrayStreamBase<float>((float[])array));
                 case TypeCode.UInt16:
-                    return new Stream(new ArrayStream_ushort((ushort[])array));
+                    return new Stream(new ArrayStreamBase<ushort>((ushort[])array));
                 case TypeCode.UInt32:
-                    return new Stream(new ArrayStream_uint((uint[])array));
+                    return new Stream(new ArrayStreamBase<uint>((uint[])array));
                 case TypeCode.UInt64:
-                    return new Stream(new ArrayStream_ulong((ulong[])array));
+                    return new Stream(new ArrayStreamBase<ulong>((ulong[])array));
                 case TypeCode.Decimal:
-                    return new Stream(new ArrayStream_decimal((decimal[])array));
+                    return new Stream(new ArrayStreamBase<decimal>((decimal[])array));
                 case TypeCode.Boolean:
-                    return new Stream(new ArrayStream_bool((bool[])array));
+                    return new Stream(new ArrayStreamBase<bool>((bool[])array));
 
             }
-
-
-            // TODO: make sure we don't have a multi-dim array
 
             throw new ArgumentException(String.Format("Unsupported array type %s", array.GetType()));
         }
 
         #endregion
 
-        #region  Instantiations of base for primitive types
-
-        // TODO: Get rid of instantiations of ArrayStreamBase.
-
-        class ArrayStream_char : ArrayStreamBase<char>
-        {
-            public ArrayStream_char(char[] array)
-                : base(array)
-            {
-            }
-        }
-
-        class ArrayStream_sbyte : ArrayStreamBase<sbyte>
-        {
-            public ArrayStream_sbyte(sbyte[] array)
-                : base(array)
-            {
-            }
-        }
-        class ArrayStream_byte : ArrayStreamBase<byte>
-        {
-            public ArrayStream_byte(byte[] array)
-                : base(array)
-            {
-            }
-        }
-        class ArrayStream_short : ArrayStreamBase<short>
-        {
-            public ArrayStream_short(short[] array)
-                : base(array)
-            {
-            }
-        }
-        class ArrayStream_int : ArrayStreamBase<int>
-        {
-            public ArrayStream_int(int[] array)
-                : base(array)
-            {
-            }
-        }
-        class ArrayStream_long : ArrayStreamBase<long>
-        {
-            public ArrayStream_long(long[] array)
-                : base(array)
-            {
-            }
-        }
-        class ArrayStream_double : ArrayStreamBase<double>
-        {
-            public ArrayStream_double(double[] array)
-                : base(array)
-            {
-            }
-        }
-        class ArrayStream_float : ArrayStreamBase<float>
-        {
-            public ArrayStream_float(float[] array)
-                : base(array)
-            {
-            }
-        }
-        class ArrayStream_ushort : ArrayStreamBase<ushort>
-        {
-            public ArrayStream_ushort(ushort[] array)
-                : base(array)
-            {
-            }
-        }
-        class ArrayStream_uint : ArrayStreamBase<uint>
-        {
-            public ArrayStream_uint(uint[] array)
-                : base(array)
-            {
-            }
-        }
-        class ArrayStream_ulong : ArrayStreamBase<ulong>
-        {
-            public ArrayStream_ulong(ulong[] array)
-                : base(array)
-            {
-            }
-        }
-
-        class ArrayStream_decimal : ArrayStreamBase<decimal>
-        {
-            public ArrayStream_decimal(decimal[] array)
-                : base(array)
-            {
-            }
-        }
-
-        class ArrayStream_bool : ArrayStreamBase<bool>
-        {
-            public ArrayStream_bool(bool[] array)
-                : base(array)
-            {
-            }
-        }
-
-        #endregion
     }
 }
