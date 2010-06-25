5fc4b13a6f220d1309b91a487609f40774dda969
David Miller <dmiller2718@gmail.com>
2010-06-25 18:03:51 -0500

    Updated to Java commit bf8bb79 (2010.04.27): added parameter destructuring support to reify and deftype/record

--- Clojure/Clojure.Source/clojure/core_deftype.clj
+++ Clojure/Clojure.Source/clojure/core_deftype.clj
@@ -45,7 +45,9 @@
                        set
                        (disj 'Object 'java.lang.Object)
                        vec)
-        methods (apply concat (vals impls))]
+        methods (map (fn [[name params & body]]
+                       (cons name (maybe-destructured params body)))
+                     (apply concat (vals impls)))]
     (when-let [bad-opts (seq (remove #{:no-print} (keys opts)))]
       (throw (ArgumentException. (apply print-str "Unsupported option(s) -" bad-opts))))   ;;; IllegalArgumentException
     [interfaces methods opts]))
