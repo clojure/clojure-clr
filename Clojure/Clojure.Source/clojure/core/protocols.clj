4b43b7587a8ca069b94745c8fec7c4c459ed6951
David Miller <dmiller2718@gmail.com>
2012-06-23 11:56:44 -0500

    Updated to Java commit df2b352 (2012.04.11): added doc string for IKVReduce

--- Clojure/Clojure.Source/clojure/core/protocols.clj
+++ Clojure/Clojure.Source/clojure/core/protocols.clj
@@ -159,4 +159,8 @@
       System.Int16 System.UInt16 System.UInt32 System.UInt64)
 
 (defprotocol IKVReduce
+  "Protocol for concrete associative types that can reduce themselves
+   via a function of key and val faster than first/next recursion over map
+   entries. Called by clojure.core/reduce-kv, and has same
+   semantics (just different arg order)."
   (kv-reduce [amap f init])) 
