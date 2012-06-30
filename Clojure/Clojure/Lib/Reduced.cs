356de6da33e1c7b331409248c6963177f65eeebb
David Miller <dmiller2718@gmail.com>
2012-06-30 11:03:41 -0500

    Updated to Java commit 4a22e3a, 4a22e3a (2012.04.30, 2012.05.02): reduce can be terminated ... + removed core.reduce for the moment ...

--- Clojure/Clojure/Lib/Reduced.cs
+++ Clojure/Clojure/Lib/Reduced.cs
@@ -1,13 +1,18 @@
-﻿// Copyright (c) Metadata Partners, LLC.
-// All rights reserved.
+﻿/**
+ *   Copyright (c) Rich Hickey. All rights reserved.
+ *   The use and distribution terms for this software are covered by the
+ *   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+ *   which can be found in the file epl-v10.html at the root of this distribution.
+ *   By using this software in any fashion, you are agreeing to be bound by
+ *    the terms of this license.
+ *   You must not remove this notice, or any other, from this software.
+ **/
 
-/* rich 4/30/12 */
-/* dmiller 6/30/12 */
+/**
+ *   Author: David Miller
+ **/
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace clojure.lang
 {
