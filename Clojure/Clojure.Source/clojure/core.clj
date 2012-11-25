bc509fd94d79ecdfb60ac6fa4461bfd4dd01dfad
David Miller <dmiller2718@gmail.com>
2012-11-25 00:22:10 -0600

    Fix method call in clojure.core/prefers

--- Clojure/Clojure.Source/clojure/core.clj
+++ Clojure/Clojure.Source/clojure/core.clj
@@ -1657,7 +1657,7 @@
   "Given a multimethod, returns a map of preferred value -> set of other values"
   {:added "1.0"
    :static true}
-  [^clojure.lang.MultiFn multifn] (.getMethodTable multifn))
+  [^clojure.lang.MultiFn multifn] (.getPreferTable multifn))
   
 ;;;;;;;;; var stuff
 
