af9d3c8154d0b0c9aaddabd9b5e716f4c8c6f610
David Miller <dmiller2718@gmail.com>
2013-02-16 11:02:58 -0600

    Fix tag declarations in clojure.clr.io/Coercion

--- Clojure/Clojure.Source/clojure/clr/io.clj
+++ Clojure/Clojure.Source/clojure/clr/io.clj
@@ -30,8 +30,8 @@
 
 (defprotocol ^{:added "1.2"} Coercions
   "Coerce between various 'resource-namish' things."
-  (^{:tag FileInfo, :added "1.2"} as-file [x] "Coerce argument to a file.")
-  (^{:tag Uri, :added "1.2"} as-uri [x] "Coerce argument to a URI."))
+  (^{:tag System.IO.FileInfo, :added "1.2"} as-file [x] "Coerce argument to a file.")
+  (^{:tag System.Uri, :added "1.2"} as-uri [x] "Coerce argument to a URI."))
 
 (extend-protocol Coercions
   nil
