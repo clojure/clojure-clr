6cdde07923f387a0f7f7ed31eed76f81068fe14a
David Miller <dmiller2718@gmail.com>
2013-03-12 17:04:43 -0500

    CLJCLR-9: Need better error message for clojure.core/cast

--- Clojure/Clojure.Source/clojure/core.clj
+++ Clojure/Clojure.Source/clojure/core.clj
@@ -322,7 +322,7 @@
   {:added "1.0"
    :static true}
   [^Type c x]   ;;; changed Class to Type
-   (if (. c (IsInstanceOfType x)) x (throw  (InvalidCastException. "Unable to cast."))))  ;;;  original (. c (cast x)))     
+   (if (. c (IsInstanceOfType x)) x (throw  (InvalidCastException. (.ToString (.GetType x))))))  ;;;  original (. c (cast x)))     
 
 (defn to-array
   "Returns an array of Objects containing the contents of coll, which
