8769f5401356d3bb6a659201a1e923bfc8da929d
David Miller <dmiller2718@gmail.com>
2013-12-03 21:40:15 -0600

    Update to Java commit 2224dba (2013.11.22): CLJ-1105: Support records in clojure.walk

--- Clojure/Clojure.Source/clojure/walk.clj
+++ Clojure/Clojure.Source/clojure/walk.clj
@@ -37,12 +37,16 @@ the sorting function."}
   functions.  Applies inner to each element of form, building up a
   data structure of the same type, then applies outer to the result.
   Recognizes all Clojure data structures. Consumes seqs as with doall."
+
   {:added "1.1"} 
   [inner outer form]
   (cond
    (list? form) (outer (apply list (map inner form)))
-   (instance? clojure.lang.IMapEntry form) (outer (vec (map inner form)))
+   (instance? clojure.lang.IMapEntry form) (outer (vec (map inner form)))  
+   (instance? System.Collections.DictionaryEntry form) (outer [(inner (.Key ^System.Collections.DictionaryEntry form)) (inner (.Value ^System.Collections.DictionaryEntry form))])
    (seq? form) (outer (doall (map inner form)))
+   (instance? clojure.lang.IRecord form)
+     (outer (reduce (fn [r x] (conj r (inner x))) form form))
    (coll? form) (outer (into (empty form) (map inner form)))
    :else (outer form)))
 
