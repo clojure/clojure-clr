dc1b113f0695e3d8fdbf24ce1f25b074dcebd216
David Miller <dmiller2718@gmail.com>
2014-11-23 18:04:18 -0600

    Updated to Java commit 18bcde4 (2014.11.14): update to latest test.generative, add test.check, reorganize test parents. Minor other mods to make all this work

---
+++ Clojure/Clojure.Tests/clojure/run_test_i.clj
@@ -0,0 +1,12 @@
+﻿(assembly-load-from "clojure.tools.namespace.dll")
+(assembly-load-from "clojure.data.generators.dll")
+(assembly-load-from "clojure.test.generative.dll")
+
+;;;(System/setProperty "java.awt.headless" "true")
+(require
+ '[clojure.test :as test]
+ '[clojure.tools.namespace.find :as ns])
+(def namespaces (ns/find-namespaces-in-dir (System.IO.DirectoryInfo. "clojure/test_clojure")))   ; java.io.File.
+(doseq [ns namespaces] (require ns))
+(let [summary (apply test/run-tests namespaces)]
+  (print summary))
