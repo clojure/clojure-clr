93289e1f518139c0215401d5afc67595584f369d
David Miller <dmiller2718@gmail.com>
2015-08-08 10:45:53 -0500

    CLJCLR-71: Convert internal task in build.proj to external for the sake of xbuild

---
+++ Clojure/BuildTasks/SetEnvVar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+
+namespace BuildTasks
+{
+    public class SetEnvVar : Task
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public override bool Execute()
+        {
+            Environment.SetEnvironmentVariable(Name, Value);
+            return true;
+        }
+    }
+}
