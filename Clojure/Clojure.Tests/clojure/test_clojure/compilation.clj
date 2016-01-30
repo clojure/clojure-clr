05c9f8bda59d98c6b4bbe6042ee181d154b9d957
David Miller <dmiller2718@gmail.com>
2016-01-30 11:51:15 -0600

    Update to Java commit a1eb406 (2015.12.02): CLJ-1845: test for direct linking

--- Clojure/Clojure.Tests/clojure/test_clojure/compilation.clj
+++ Clojure/Clojure.Tests/clojure/test_clojure/compilation.clj
@@ -385,3 +385,15 @@
 (deftest test-anon-recursive-fn
   (is (= [0 0] (take 2 ((fn rf [x] (lazy-seq (cons x (rf x)))) 0))))
   (is (= [0 0] (take 2 (zf 0)))))
+
+;; See CLJ-1845
+(deftest direct-linking-for-load
+  (let [called? (atom nil)
+        logger (fn [& args]
+                 (reset! called? true)
+                 nil)]
+    (with-redefs [load logger]
+      ;; doesn't actually load clojure.repl, but should
+      ;; eventually call `load` and reset called?.
+      (require 'clojure.repl :reload))
+    (is @called?)))
