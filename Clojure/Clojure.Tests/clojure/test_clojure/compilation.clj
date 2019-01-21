e09beb6b41973cadf1ba4984077c956451193c74
David Miller <dmiller2718@gmail.com>
2019-01-21 14:41:15 -0600

    Update to Java commit d30542e (2018.10.05): Revert "CLJ-1550: define package for class in DynamicClassLoader"

--- Clojure/Clojure.Tests/clojure/test_clojure/compilation.clj
+++ Clojure/Clojure.Tests/clojure/test_clojure/compilation.clj
@@ -435,10 +435,6 @@
       (require 'clojure.repl :reload))
     (is @called?)))
 
-;;;(deftest CLJ-1550-dcl-package             -- not relevant
-;;;  (let [pkg (.getPackage clojure.test_clojure.compilation.load_ns.x)]
-;;;    (is (= "clojure.test_clojure.compilation.load_ns" (and pkg (.getName pkg))))))
-
 ;;;(deftest clj-1714                        -- not relevant
 ;;;  (testing "CLJ-1714 Classes shouldn't have their static initialisers called simply by type hinting or importing"
 ;;;    ;; ClassWithFailingStaticInitialiser will throw if its static initialiser is called
