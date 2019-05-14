98f71926394d6c51e3fd6da9c6c3c0bd1748c63f
dmiller <dmiller2718@gmail.com>
2019-05-14 14:25:16 -0500

    Make run_test work with clojure.test-clojure.exclude-namespaces env var not set Had done this a long time ago for run_test_i, missed duplicating that here.

--- Clojure/Clojure.Tests/clojure/run_test.clj
+++ Clojure/Clojure.Tests/clojure/run_test.clj
@@ -7,8 +7,7 @@
 (require
  '[clojure.test :as test]
  '[clojure.tools.namespace.find :as ns])
-(def namespaces (remove (read-string (System.Environment/GetEnvironmentVariable "clojure.test-clojure.exclude-namespaces"))  ;;; System/getProperty
-                        (ns/find-namespaces-in-dir (System.IO.DirectoryInfo. "clojure/test_clojure"))))                      ;;; (java.io.File. "test")
-(doseq [ns namespaces] (require ns))
+(def namespaces (remove (read-string (or (System.Environment/GetEnvironmentVariable "clojure.test-clojure.exclude-namespaces") "#{}"))    ;;; System/getProperty  Added the or
+                        (ns/find-namespaces-in-dir (System.IO.DirectoryInfo. "clojure/test_clojure"))))                                   ;;; (java.io.File. "test")(doseq [ns namespaces] (require ns))
 (let [summary (apply test/run-tests namespaces)]
   (Environment/Exit (if (test/successful? summary) 0 -1)))   ;;; System/exit
