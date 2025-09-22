c6b069e8de9b9e29956728593f844b20804ed8d0
David Miller <dmiller2718@gmail.com>
2025-09-21 21:14:47 -0400

    Improve lookup of trusted framework assemblies to use actual path in Assembly.LoadFrom.

--- Clojure/Clojure/Lib/RT.cs
+++ Clojure/Clojure/Lib/RT.cs
@@ -2758,19 +2758,37 @@ namespace clojure.lang
 
         #region Locating types
 
-        // Cache for all runtime library assembly names, loaded once on demand.
-        private static readonly Lazy<List<AssemblyName>> _runtimeAssemblyNames = new(() =>
+
+        class CaseInsensitiveAssemblyNameComparer : IEqualityComparer<AssemblyName>
         {
-            var names = new List<AssemblyName>();
+            public bool Equals(AssemblyName x, AssemblyName y)
+            {
+                return string.Equals(x.Name, y.Name, StringComparison.OrdinalIgnoreCase);
+            }
+
+            public int GetHashCode(AssemblyName obj)
+            {
+                return obj.Name.ToLowerInvariant().GetHashCode();
+            }
+        }
+
+
+        // Cache for all runtime library assembly names, loaded once on demand.
+        // Key is the assembly name.  Value is either the path or empty string.
+        // If non-empty can use Assembly.LoadFrom(path) to load.
+        // If empty, must use Assembly.Load(name).
+
+
+        private static readonly Lazy<Dictionary<AssemblyName, string>> _runtimeAssemblyNames = new(() =>
+        {
+            Dictionary<AssemblyName, string> names = new(new CaseInsensitiveAssemblyNameComparer());
 
             try
             {
                 // DependencyContext.Default can be null in some scenarios (like unit tests or static initializers).
                 // Loading the context from a known assembly is more robust.
-
                 var entryAssembly = Assembly.GetEntryAssembly() ?? typeof(RT).Assembly;
                 var context = Microsoft.Extensions.DependencyModel.DependencyContext.Load(entryAssembly);
-
                 if (context != null)
                 {
                     foreach (var lib in context.RuntimeLibraries)
@@ -2780,7 +2798,7 @@ namespace clojure.lang
                             try
                             {
                                 var name = new AssemblyName(Path.GetFileNameWithoutExtension(assembly));
-                                names.Add(name);
+                                names[name] = string.Empty;
                             }
                             catch { }
                         }
@@ -2789,35 +2807,87 @@ namespace clojure.lang
             }
             catch { }
 
-            // Also include shared runtime libraries from TRUSTED_PLATFORM_ASSEMBLIES
-            try
-            {
+
 #if (NET48_OR_GREATER || !NETFRAMEWORK)
-                var trustedAssemblies = AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES") as string;
+            var trustedAssemblies = AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES") as string;
 #else
                 var trustedAssemblies = "";
 #endif
-                if (!string.IsNullOrEmpty(trustedAssemblies))
+            if (!string.IsNullOrEmpty(trustedAssemblies))
+            {
+                var paths = trustedAssemblies.Split(Path.PathSeparator);
+
+                foreach (var path in paths)
                 {
-                    var paths = trustedAssemblies.Split(Path.PathSeparator);
-                    foreach (var path in paths)
-                    {
-                        try
-                        {
-                            var name = AssemblyName.GetAssemblyName(path);
-                            if (!names.Any(n => n.Name == name.Name))
-                            {
-                                names.Add(name);
-                            }
-                        }
-                        catch { }
-                    }
+                    names[new AssemblyName(Path.GetFileNameWithoutExtension(path))] = path;
                 }
             }
-            catch { }
 
             return names;
         });
+
+
+
+        //        // Cache for all runtime library assembly names, loaded once on demand.
+        //        private static readonly Lazy<List<AssemblyName>> _runtimeAssemblyNames = new(() =>
+        //        {
+        //            var names = new List<AssemblyName>();
+
+        //            try
+        //            {
+        //                // DependencyContext.Default can be null in some scenarios (like unit tests or static initializers).
+        //                // Loading the context from a known assembly is more robust.
+
+        //              var entryAssembly = Assembly.GetEntryAssembly() ?? typeof(RT).Assembly;
+        //                var context = Microsoft.Extensions.DependencyModel.DependencyContext.Load(entryAssembly);
+
+        //                if (context != null)
+        //                {
+        //                    foreach (var lib in context.RuntimeLibraries)
+        //                    {
+        //                        foreach (var assembly in lib.RuntimeAssemblyGroups.SelectMany(g => g.AssetPaths))
+        //                        {
+        //                            try
+        //                            {
+        //                                var name = new AssemblyName(Path.GetFileNameWithoutExtension(assembly));
+        //                                names.Add(name);
+        //                            }
+        //                            catch { }
+        //                        }
+        //                    }
+        //                }
+        //            }
+        //            catch { }
+
+        //            // Also include shared runtime libraries from TRUSTED_PLATFORM_ASSEMBLIES
+        //            try
+        //            {
+        //#if (NET48_OR_GREATER || !NETFRAMEWORK)
+        //                var trustedAssemblies = AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES") as string;
+        //#else
+        //                var trustedAssemblies = "";
+        //#endif
+        //                if (!string.IsNullOrEmpty(trustedAssemblies))
+        //                {
+        //                    var paths = trustedAssemblies.Split(Path.PathSeparator);
+        //                    foreach (var path in paths)
+        //                    {
+        //                        try
+        //                        {
+        //                            var name = AssemblyName.GetAssemblyName(path);
+        //                            if (!names.Any(n => n.Name == name.Name))
+        //                            {
+        //                                names.Add(name);
+        //                            }
+        //                        }
+        //                        catch { }
+        //                    }
+        //                }
+        //            }
+        //            catch { }
+
+        //            return names;
+        //        });
 
         internal static readonly char[] _triggerTypeChars = ['`', ',', '[', '&', '*'];
 
@@ -2826,12 +2896,14 @@ namespace clojure.lang
             return classForName(p, CurrentNSVar.deref() as Namespace, true);
         }
 
+        // Leaving in this metering code for now.
+        // Remove after we're done debugging.
+
         public static int NumDuplicates { get; set; } = 0;
         public static int NumDirects { get; set; } = 0;
         public static int NumLoadedAssemblyFinds { get; set; } = 0;
         public static int NumRuntimeAssemblyLoads { get; set; } = 0;
         public static int NumRuntimeAssemblyFinds { get; set; } = 0;
-        public static int NumLoadedAssemblyFinds2 { get; set; } = 0;
         public static int NumParsing { get; set; } = 0;
         public static int NumFails { get; set; } = 0;
 
@@ -2842,10 +2914,8 @@ namespace clojure.lang
             Console.WriteLine($"NumLoadedAssemblyFinds:  {NumLoadedAssemblyFinds}");
             Console.WriteLine($"NumRuntimeAssemblyLoads: {NumRuntimeAssemblyLoads}");
             Console.WriteLine($"NumRuntimeAssemblyFinds: {NumRuntimeAssemblyFinds}");
-            Console.WriteLine($"NumLoadedAssemblyFinds2: {NumLoadedAssemblyFinds2}");
             Console.WriteLine($"NumParsing:              {NumParsing}");
             Console.WriteLine($"NumFails:                {NumFails}");
-
         }
 
         internal static Type classForName(string p, Namespace ns, bool canCallClrTypeSpec)
@@ -2896,53 +2966,35 @@ namespace clojure.lang
 
                 if (!string.IsNullOrEmpty(namespaceName))
                 {
-
                     var runtimeAssemblyNames = _runtimeAssemblyNames.Value;
-                    if (runtimeAssemblyNames.Count > 0)
+
+                    var targetAssemblyName = new AssemblyName(namespaceName);
+
+                    // try if the namespace directly matches the assembly name.
+                    if (runtimeAssemblyNames.TryGetValue(targetAssemblyName, out var path))
                     {
 
-                        // Try to find and load the assembly that might contain this type.
-                        foreach (var assemblyName in runtimeAssemblyNames)
+                        if (loadedAssemblies.Any(a => a.GetName().Name.Equals(namespaceName, StringComparison.OrdinalIgnoreCase)))
                         {
                             // Skip if this assembly is already loaded.
-                            if (loadedAssemblies.Any(a => a.GetName().Name == assemblyName.Name))
+                        }
+                        else
+                        {
+                            NumRuntimeAssemblyLoads++;
+                            var assy = string.IsNullOrWhiteSpace(path) ? Assembly.Load(targetAssemblyName) : Assembly.LoadFrom(path);
+                            var type = assy.GetType(p, false);
+                            if (type != null && (type.IsPublic || type.IsNestedPublic))
                             {
-                                continue;
+                                NumRuntimeAssemblyFinds++;
+                                return type;
                             }
-
-                            // try if the namespace directly matches the assembly name.
-                            if (assemblyName.Name.Equals(namespaceName, StringComparison.OrdinalIgnoreCase))
-                            {
-                                try
-                                {
-                                    NumRuntimeAssemblyLoads++;
-                                    var assy = Assembly.Load(assemblyName);
-                                    var type = assy.GetType(p, false);
-                                    if (type != null && (type.IsPublic || type.IsNestedPublic))
-                                    {
-                                        NumRuntimeAssemblyFinds++;
-                                        return type;
-                                    }
-                                }
-                                catch { }
-                            }
-
                         }
                     }
                 }
             }
 
-            //// Re-check loaded assemblies (some might have been loaded during the search).
-            //loadedAssemblies = domain.GetAssemblies();
-            //foreach (Assembly assy in loadedAssemblies)
-            //{
-            //    Type t1 = assy.GetType(p, false);
-            //    if (t1 != null && (t1.IsPublic || t1.IsNestedPublic))
-            //    {
-            //        NumLoadedAssemblyFinds2++;
-            //        return t1;
-            //    }
-            //}
+
+
 
             // Search by simple type name (slow path).
             // At some point this became a no-op unless running on Mono, so I restricted it to that.
